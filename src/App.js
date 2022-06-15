4393754ef0ad746bae4161fd495a64cf8adf549b
‘AMAYA <aamaya547@gmail.com>
2022-06-15 15:02:47 -0700

    goblin arrays appearing in the console

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import './App.css';
 import GoblinForm from './GoblinForm';
 import GoblinList from './GoblinList';
 import Goblin from './Goblin';
+import { useState } from 'react';
 
 function App() {
+  const [goblinFormName, setGoblinFormName] = useState('');
+  const [goblinFormColor, setGoblinFormColor] = useState('');
+  const [goblinFormHP, setGoblinFormHP] = useState(0);
+  const [allGoblins, setAllGoblins] = useState([{ name: 'Chedder', color: 'green', HP: 50 }]);
+
+  // console.log(goblinFormName, goblinFormHP, goblinFormColor);
+  console.log(allGoblins);
+
   /* 
     track: 
       allGoblins, an array of all goblins
@@ -15,9 +24,9 @@ function App() {
 
   function submitGoblin(e) {
     e.preventDefault();
-
+    const newGoblin = { name: goblinFormName, color: goblinFormColor, HP: goblinFormHP };
     // on submit, make a new goblin object with a name that comes from the form state, an hp that comes from the form state, and a color that comes from the form state
-
+    setAllGoblins([...allGoblins, newGoblin]);
     // update the allGoblins array. Add the new goblin to the allGoblins array immutably.
 
     // clear out the goblin form state items by setting them to empty strings. This will cause the form to reset in the UI.
@@ -55,16 +64,18 @@ function App() {
         <input onChange={(e) => handleFilterGoblins(e.target.value)} />
       </div>
       <GoblinForm
-      /*
+        submitGoblin={submitGoblin}
+        goblinFormName={goblinFormName}
+        setGoblinFormName={setGoblinFormName}
+        goblinFormColor={goblinFormColor}
+        setGoblinFormColor={setGoblinFormColor}
+        goblinFormHP={goblinFormHP}
+        setGoblinFormHP={setGoblinFormHP}
+        /*
         This component takes in a ton of props! 
         Here is the list of props to pass:
           submitGoblin,
-          goblinFormName, 
-          setGoblinFormName,
-          goblinFormColor, 
-          setGoblinFormColor,
-          goblinFormHP, 
-          setGoblinFormHP,
+         **everything above will be tracked in state up top**
         */
       />
       <GoblinList
